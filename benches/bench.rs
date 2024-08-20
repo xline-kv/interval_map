920d6c098428d8783e3e6d33c4b64b56eb9e76c2
feathercyc <feathercyc@163.com>
2024-08-20 12:54:36 +0000

    feat: add some iter types
    Signed-off-by: feathercyc <feathercyc@163.com>

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,44 +1,24 @@
 use criterion::{criterion_group, criterion_main, Bencher, Criterion};
 use interval_map::{Interval, IntervalMap};
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use std::hint::black_box;
 
-struct Rng {
-    state: u32,
-}
-impl Rng {
-    fn new() -> Self {
-        Self { state: 0x87654321 }
-    }
-
-    fn gen_u32(&mut self) -> u32 {
-        self.state ^= self.state << 13;
-        self.state ^= self.state >> 17;
-        self.state ^= self.state << 5;
-        self.state
-    }
-
-    fn gen_range_i32(&mut self, low: i32, high: i32) -> i32 {
-        let d = (high - low) as u32;
-        low + (self.gen_u32() % d) as i32
-    }
-}
-
 struct IntervalGenerator {
-    rng: Rng,
-    limit: i32,
+    rng: StdRng,
+    limit: u32,
 }
 impl IntervalGenerator {
     fn new() -> Self {
-        const LIMIT: i32 = 100000;
+        const LIMIT: u32 = 1000;
         Self {
-            rng: Rng::new(),
+            rng: StdRng::from_seed([0; 32]),
             limit: LIMIT,
         }
     }
 
-    fn next(&mut self) -> Interval<i32> {
-        let low = self.rng.gen_range_i32(0, self.limit - 1);
-        let high = self.rng.gen_range_i32(low + 1, self.limit);
+    fn next(&mut self) -> Interval<u32> {
+        let low = self.rng.gen_range(0..=self.limit - 1);
+        let high = self.rng.gen_range(low + 1..=self.limit);
         Interval::new(low, high)
     }
 }
@@ -100,14 +80,68 @@ fn bench_interval_map_insert_remove(c: &mut Criterion) {
     });
 }
 
+// FilterIter helper fn
+fn interval_map_filter_iter(count: usize, bench: &mut Bencher) {
+    let mut gen = IntervalGenerator::new();
+    let intervals: Vec<_> = std::iter::repeat_with(|| gen.next()).take(count).collect();
+    let mut map = IntervalMap::new();
+    for i in intervals.clone() {
+        map.insert(i, ());
+    }
+    bench.iter(|| {
+        for i in intervals.clone() {
+            black_box(map.filter_iter(&i).collect::<Vec<_>>());
+        }
+    });
+}
+
+// iter().filter() helper fn
+fn interval_map_iter_filter(count: usize, bench: &mut Bencher) {
+    let mut gen = IntervalGenerator::new();
+    let intervals: Vec<_> = std::iter::repeat_with(|| gen.next()).take(count).collect();
+    let mut map = IntervalMap::new();
+    for i in intervals.clone() {
+        map.insert(i, ());
+    }
+    bench.iter(|| {
+        for i in intervals.clone() {
+            black_box(map.iter().filter(|v| v.0.overlaps(&i)).collect::<Vec<_>>());
+        }
+    });
+}
+
+fn bench_interval_map_filter_iter(c: &mut Criterion) {
+    c.bench_function("bench_interval_map_filter_iter_100", |b| {
+        interval_map_filter_iter(100, b)
+    });
+    c.bench_function("bench_interval_map_filter_iter_1000", |b| {
+        interval_map_filter_iter(1000, b)
+    });
+}
+
+fn bench_interval_map_iter_filter(c: &mut Criterion) {
+    c.bench_function("bench_interval_map_iter_filter_100", |b| {
+        interval_map_iter_filter(100, b)
+    });
+    c.bench_function("bench_interval_map_iter_filter_1000", |b| {
+        interval_map_iter_filter(1000, b)
+    });
+}
+
 fn criterion_config() -> Criterion {
     Criterion::default().configure_from_args().without_plots()
 }
 
 criterion_group! {
-    name = benches;
+    name = benches_basic_op;
     config = criterion_config();
-    targets = bench_interval_map_insert, bench_interval_map_insert_remove
+    targets = bench_interval_map_insert, bench_interval_map_insert_remove,
 }
 
-criterion_main!(benches);
+criterion_group! {
+    name = benches_iter;
+    config = criterion_config();
+    targets = bench_interval_map_filter_iter, bench_interval_map_iter_filter
+}
+
+criterion_main!(benches_basic_op, benches_iter);
